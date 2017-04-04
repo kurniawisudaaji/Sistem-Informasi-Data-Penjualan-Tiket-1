24a93b5a523af3cbe04caea6188466d97b4f93cd
kurniawisudaaji <kurniawisudaaji0@gmail.com>
2017-04-04 18:39:45 +0700

    Update tiket+kereta

--- TUBES/SisfoPenjualanTiket/src/sisfopenjualantiket/Kereta.java
+++ TUBES/SisfoPenjualanTiket/src/sisfopenjualantiket/Kereta.java
@@ -5,10 +5,78 @@
  */
 package sisfopenjualantiket;
 
+import java.util.ArrayList;
+
 /**
  *
  * Kurnia Wisuda Aji (1301150068)
  */
 public class Kereta {
+    private ArrayList<Gerbong> daftarGerbong;
+    private String nama;
+    private String jenis;
+    private String jurusan;
+
+    public Kereta(String nama, String jenis, String jurusan) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.jurusan = jurusan;
+        daftarGerbong = new ArrayList<>();
+    }
+
+    
+    public Gerbong getGerbongbyIndex(int x) {
+        return this.daftarGerbong.get(x);
+    }
+
+    
+    public void addDaftarGerbong(Gerbong daftar_gerbong) {
+//        daftar_gerbong = new Gerbong();
+        this.daftarGerbong.add(this.daftarGerbong.size(), daftar_gerbong);
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+    
+    public void removeGerbong(String kode) {
+        for (int i= 0; i<this.daftarGerbong.size();i++) {
+            if (this.getGerbongbyIndex(i).getKode().equals(kode)) {
+                this.daftarGerbong.remove(i);
+            }
+        }
+        
+    }
+    
+    public int getNGerbong() {
+        return this.daftarGerbong.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Kereta{" + "daftarGerbong=" + daftarGerbong + ", nama=" + nama + ", jenis=" + jenis + ", jurusan=" + jurusan + '}';
+    }
+
     
 }
+
--- TUBES/SisfoPenjualanTiket/src/sisfopenjualantiket/Tiket.java
+++ TUBES/SisfoPenjualanTiket/src/sisfopenjualantiket/Tiket.java
@@ -5,10 +5,98 @@
  */
 package sisfopenjualantiket;
 
+import java.util.Date;
+
 /**
  *
  * Kurnia Wisuda Aji (1301150068)
  */
 public class Tiket {
-    
+
+    private int kode_tiket;
+    private String nama_kereta;
+    private String jenis;
+    private Date tanggal;
+    private Rute rute;
+    private long harga;
+
+    public Tiket(int kode_tiket, String nama_kereta, String jenis, Date tanggal, Rute rute, long harga) {
+        this.kode_tiket = kode_tiket;
+        this.nama_kereta = nama_kereta;
+        this.jenis = jenis;
+        this.tanggal = tanggal;
+        this.rute = rute;
+        this.harga = harga;
+    }
+
+    public int getKode_tiket() {
+        return kode_tiket;
+    }
+
+    public void setKode_tiket(int kode_tiket) {
+        this.kode_tiket = kode_tiket;
+    }
+
+    public String getNama_kereta() {
+        return nama_kereta;
+    }
+
+    public void setNama_kereta(String nama_kereta) {
+        this.nama_kereta = nama_kereta;
+    }
+
+    public String getJenis() {
+        if (jenis.equals("Exc")) {
+            return ("Executive");
+        } else if (jenis.equals("Bis")) {
+            return ("Business");
+        } else if (jenis.equals("Eco")) {
+            return ("Economy");
+        } else {
+            return ("Salah gerbong bosku, cek kelasnya lagi coba ya^^");
+        }
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public Date getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(Date tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public Rute getRute() {
+        return rute;
+    }
+
+    public void setRute(Rute rute) {
+        this.rute = rute;
+    }
+
+    public long getHarga() {
+        return harga;
+    }
+
+    public void setHarga(long harga) {
+        this.harga = harga;
+    }
+
+    @Override
+    public String toString() {
+        return "Tiket{" + "kode_tiket=" + kode_tiket + ", nama_kereta=" + nama_kereta + ", jenis=" + jenis + ", tanggal=" + tanggal + ", rute=" + rute + ", harga=" + harga + '}';
+    }
+
+    public void display() {
+        System.out.println("Nomor ID     : " + getKode_tiket());
+        System.out.println("Nama KA      : " + getNama_kereta());
+        System.out.println("Jenis        : " + getJenis());
+        System.out.println("Rute         : " + getRute().toString());
+        System.out.println("Tanggal      : " + getTanggal());
+        System.out.println("Harga        : " + getHarga());
+    }
+
 }
